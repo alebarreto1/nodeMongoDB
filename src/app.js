4d8594e3f3ee1846660ac752c3a897dd411f336a
Alessandro Rodrigo <88581684+alebarreto1@users.noreply.github.com>
2022-04-09 12:11:16 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexão.'));
 db.once("open", () => {
-    console.log("Conexão com MongoDB feita com sucesso!")
+    console.log("Conexão com MongoDB feita com sucesso!");
 })
 
 const app = express();
